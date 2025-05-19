83da1e49af03ab9750b61660c236dd046394c9a6
YorikoSC <47029214+YorikoSC@users.noreply.github.com>
2025-05-19 18:34:02 +0300

    Update special_wtf.js

--- chapters/special/special_wtf.js
+++ chapters/special/special_wtf.js
@@ -21,32 +21,61 @@ export default {
 	const texts = gameState.language === 'en' ? {
 		msg2: "Nah, for real, what’s the deal?",
 		msg3: "Okay, I’ll drop it. Have fun at the café.",
+		msg3a: "Aww, you’re the best! Nighty-night, my cutie! 😘",
+		msg3b: "Whatever, enjoy your café... I’m out for now. Night.",
 		msg4: "Fuck this, I’m done. Don’t talk to me tonight.",
+		msg4a: "Whoa, babe, chill! Okay, I’ll back off. Sleep it off, we’ll talk later, k? 😔",
         } : {
 		msg2: "Нет, серьёзно, в чём дело?",
 		msg3: "Ладно, проехали. Веселись в кафе.",
-		msg4: "Пиздец, всё, отвали. Не пиши мне сегодня."
+		msg3a: "Спасибо, милый! Ты топ! Споки-споки! 😘",
+		msg3b: "не уверен..",
+		msg4: "Пиздец, всё, отвали. Не пиши мне сегодня.",
+		msg4a: "Да ладно тебе",
 
         };       
 	return [
         	{
                 id: "really",
-                choice1: text.msg2,
+                choice1: texts.msg2,
 		delay:1500,
                 nextChapter: "special_really"
 		},
 		{
                 id: "forget",
-                choice2: text.msg3,
-		delay:1500,
-                nextChapter: "special_forget"
-		},
+                choice2: texts.msg3,
+		delay:1500
+		result: [
+          		{
+            		type: "received",
+            		text: texts.msg3a,
+            		delay: 1700
+          		},
+          		{
+            		id: "3b",
+            		text: texts.msg3b,
+            		delay: 2100,
+            		nextChapter: 'special_end'
+          		}
+		];
 		{
                 id: "fuckoff",
-                choice3: text.msg4,
-		delay:1500,
-                nextChapter: "special_fuckoff"
-		}
-        ];
+                choice3: texts.msg4,
+		delay:1500
+		result: [
+          		{
+            		type: "received",
+            		text: texts.msg4a,
+            		delay: 1700
+          		},
+          		{
+            		id: "3b",
+            		text: texts.msg3b,
+            		delay: 2100,
+            		nextChapter: 'special_end'
+          		}
+		];
 	}
+        ];
+}
 }
