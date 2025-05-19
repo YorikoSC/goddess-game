2d2d253b06aa0366c3871fc3f68921bc13cf0ae6
YorikoSC <47029214+YorikoSC@users.noreply.github.com>
2025-05-19 23:08:27 +0300

    Create special_really_tomorrow.js

---
+++ chapters/special/special_really_tomorrow.js
@@ -0,0 +1,30 @@
+export default {
+  getText(gameState) {
+    const texts = gameState.language === 'en' ? {
+      msg1: "That's an awesome attitude! 😘",
+    } : {
+      msg1: "Классный настрой! 😘",
+    };
+
+    return [
+      {
+        type: "received",
+        text: texts.msg1,
+        delay: 1000
+      }
+    ];
+  },
+  getChoices(gameState) {
+    const texts = gameState.language === 'en' ? {
+      choice1: "I'm not sure.",
+     } : {
+      choice1a: "не уверен..",
+      return [
+      {
+        id: "choice1",
+        text: texts.choice1,
+        delay: 1500,
+        nextChapter: 'special_end'
+      }
+  ];
+}
